083b6680e97cfc2007e3a898b45511c3c7389813
Shardendu Mishra <152168607+ShardenduMishra22@users.noreply.github.com>
2024-11-03 11:51:58 +0530

    first commit

---
+++ backend/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Writting Hello
+	fmt.Println("This is My News Aggregator Web Scrapper Project!!")
+}
---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Routes, Route } from 'react-router-dom';
+// import Home from './pages/Home';
+
+function App() {
+  return (
+    <Routes>
+      {/* <Route path="/" element={<Home />} /> */}
+    </Routes>
+  );
+}
+
+export default App;
